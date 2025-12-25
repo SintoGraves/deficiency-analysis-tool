eb830e3387e2e65158a94b5be0799711bea628eb
SintoGraves <sintograves@gmail.com>
2025-12-25 10:19:09 -0500

    Create glossary.js

---
+++ docs/js/data/glossary.js
@@ -0,0 +1,20 @@
+/*-------------------------------------------------
+ * /docs/js/data/glossary.js
+ * Static glossary for Notes/Directives
+ * Namespace: window.DDT
+ *-------------------------------------------------*/
+(function () {
+  const DDT = (window.DDT = window.DDT || {});
+  DDT.GLOSSARY = {
+    SUT:   "System Under Test",
+    OMF:   "Operational Mission Failure",
+    OPCON: "Operational Consideration",
+    TACON: "Tactical Consideration",
+    CPD:   "Capability Production Document",
+    CDD:   "Capability Development Document",
+    TTP:   "Tactics, Techniques, and Procedures",
+    CONOP: "Concept of Operations",
+    CONOPS:"Concept of Operations",
+    PMS:   "Preventative Maintenance System"
+  };
+})();
